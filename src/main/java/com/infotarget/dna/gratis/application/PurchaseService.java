d6e74dc7500a44d6ada8368e8c2edae4f08a35d7
tomaszmucha <tmucha82@gmail.com>
2020-05-12 22:01:56 +0200

    small refactor

--- src/main/java/com/infotarget/dna/gratis/application/PurchaseService.java
+++ src/main/java/com/infotarget/dna/gratis/application/PurchaseService.java
@@ -7,6 +7,8 @@ import com.infotarget.dna.gratis.domain.PurchaseId;
 import com.infotarget.dna.gratis.domain.PurchaseRepository;
 import com.infotarget.dna.shared.DomainEventPublisher;
 import com.infotarget.dna.shared.Result;
+import io.vavr.Function2;
+import io.vavr.Function3;
 
 import static java.lang.String.format;
 
@@ -23,44 +25,31 @@ public class PurchaseService {
     }
 
     public Result addProduct(PurchaseId purchaseId, Product product) {
-        Purchase purchase = purchaseRepository.findBy(purchaseId).orElse(null);
-        if (purchase == null) {
-            return Result.failure(format("Cannot find purchase with id = %s", purchaseId));
-        }
-        Result result = purchase.addProduct(product, gratisPolicy);
-        result.events().forEach(domainEventPublisher::publish);
-        purchaseRepository.update(purchase);
-        return result;
+        return execute(purchaseId, product, Purchase::addProduct);
     }
 
     public Result removeProduct(PurchaseId purchaseId, Product product) {
-        Purchase purchase = purchaseRepository.findBy(purchaseId).orElse(null);
-        if (purchase == null) {
-            return Result.failure(format("Cannot find purchase with id = %s", purchaseId));
-        }
-        Result result = purchase.removeProduct(product, gratisPolicy);
-        result.events().forEach(domainEventPublisher::publish);
-        purchaseRepository.update(purchase);
-        return result;
+        return execute(purchaseId, product, Purchase::removeProduct);
     }
 
     public Result removeGratisOnDemand(PurchaseId purchaseId, Product product) {
-        Purchase purchase = purchaseRepository.findBy(purchaseId).orElse(null);
-        if (purchase == null) {
-            return Result.failure(format("Cannot find purchase with id = %s", purchaseId));
-        }
-        Result result = purchase.removeGratis(product);
-        result.events().forEach(domainEventPublisher::publish);
-        purchaseRepository.update(purchase);
-        return result;
+        return execute(purchaseId, product, Purchase::removeGratis);
     }
 
     public Result addGratisAgain(PurchaseId purchaseId, Product product) {
+        return execute(purchaseId, product, Purchase::addGratisAgain);
+    }
+
+    private  Result execute(PurchaseId purchaseId, Product product, Function2<Purchase, Product, Result> supplier) {
+        return execute(purchaseId, product, (pu, pr, gr) -> supplier.apply(pu, pr));
+    }
+
+    private  Result execute(PurchaseId purchaseId, Product product, Function3<Purchase, Product, GratisPolicy, Result> operation) {
         Purchase purchase = purchaseRepository.findBy(purchaseId).orElse(null);
         if (purchase == null) {
             return Result.failure(format("Cannot find purchase with id = %s", purchaseId));
         }
-        Result result = purchase.addGratisAgain(product);
+        Result result = operation.apply(purchase, product, gratisPolicy);
         result.events().forEach(domainEventPublisher::publish);
         purchaseRepository.update(purchase);
         return result;
